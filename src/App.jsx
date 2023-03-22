00d642da717392f3936eb99c730e8cc4072a3fa2
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-03-22 06:45:42 -0700

    uncomment out import of react router dom in app.jsx to merge... previous commit was unsaved

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/Header/Header";
 
