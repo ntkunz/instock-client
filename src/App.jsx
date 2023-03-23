8edcdd6f6e738be5f3a1b0f952d2a4963ac50d7a
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-03-23 13:58:59 -0700

    remove text align center from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/Header/Header";
-import AddNewInventoryItem from "./components/AddNewInventoryItem/AddNewInventoryItem";
-import AddNewWarehouse from "./components/AddNewWarehouse/AddNewWarehouse";
-import WarehouseDetails from "./components/WarehouseDetails/WarehouseDetails";
-import ContactDetails from "./components/ContactDetails/ContactDetails";
+// import AddNewInventoryItem from "./components/AddNewInventoryItem/AddNewInventoryItem";
+// import AddNewWarehouse from "./components/AddNewWarehouse/AddNewWarehouse";
+// import WarehouseDetails from "./components/WarehouseDetails/WarehouseDetails";
+// import ContactDetails from "./components/ContactDetails/ContactDetails";
 import Footer from "./components/Footer/Footer";
 import AddNewWarehouseForm from "./components/AddNewWarehouseForm/AddNewWarehouseForm";
 export default function App() {
