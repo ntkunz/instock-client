2b2b4b0d890f0b895079eb4304f2a213c3b3c156
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-03-23 16:25:30 -0700

    create checkbox and dropdown input on itemAvailabilityForm.jsx component

--- src/components/ItemAvailabilityForm/ItemAvailabilityForm.jsx
+++ src/components/ItemAvailabilityForm/ItemAvailabilityForm.jsx
@@ -3,31 +3,37 @@ import "./ItemAvailabilityForm.scss";
 function ItemAvailabilityForm() {
     return (
         <div className="avail">
-            <h2 className="avail__title">Item avail</h2>
-            <label htmlFor="name" className="avail__label">Item Name</label>
-            <input 
+            <h2 className="avail__title">Item Availability</h2>
+            <label htmlFor="name" className="avail__label">Status</label>
+            {/* <fieldset className="avail__status"> */}
+            <div className="avail__radio-container">
+                <div className="avail__radio-set">
+                  <input className="avail__radio" type="radio" id="instock" name="instock" value="huey" checked />
+                  <label className="avail__radio-label" for="instock">In stock</label>
+                </div>
+                <div className="avail__radio-set">
+                  <input className="avail__radio" type="radio" id="outofstock" name="instock" value="outofstock" />
+                  <label className="avail__radio-label out-of-stock" for="outofstock">Out of stock</label>
+                </div>
+            </div>
+            {/* </fieldset> */}
+            <label htmlFor="quantity" className="avail__label">Quantity</label>
+            <input
                 type="text" 
                 className="avail__input" 
-                // value={state.itemName}
+                // value={state.quantity}
                 // onChange={ handleChange }
-                placeholder="Item Name"
+                placeholder="" 
             />
-            <label htmlFor="desc" className="avail__label">Description</label>
-            <textarea 
-                type="text" 
-                className="avail__desc-input" 
-                // value={state.desc}
-                // onChange={ handleChange }
-                placeholder="Please enter a brief item description..." 
-            />
-            <label htmlFor="" className="avail__label">Category</label>
-            <input 
-                type="text" 
-                className="avail__input" 
-                // value={state.category}
-                // onChange={ handleChange }
-                placeholder="Item Name"
-            />
+            <label htmlFor="" className="avail__label">Warehouse</label>
+                <select 
+                    className="avail__warehouse" 
+                    name="avail_warehouse" 
+                    id="avail_warehouse"
+                >
+                    <option value="volvo">Volvo</option>
+                    <option value="saab">Saab</option>
+                </select>
         </div>
     )
 }
