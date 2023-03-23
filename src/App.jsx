b8e457afeb5a1a584d648981d2cc9dd9561e718b
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-03-23 14:04:46 -0700

    add new inventory item added to return on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/Header/Header";
-// import AddNewInventoryItem from "./components/AddNewInventoryItem/AddNewInventoryItem";
+import AddNewInventoryItem from "./components/AddNewInventoryItem/AddNewInventoryItem";
 // import AddNewWarehouse from "./components/AddNewWarehouse/AddNewWarehouse";
 // import WarehouseDetails from "./components/WarehouseDetails/WarehouseDetails";
 // import ContactDetails from "./components/ContactDetails/ContactDetails";
@@ -12,7 +12,7 @@ export default function App() {
     <div className="App">
       <BrowserRouter>
       <Header />
-      <AddNewWarehouseForm />
+      <AddNewInventoryItem />
       <Footer />
         {/* <Routes>
           <Route path="/" element={<WarehouseList />} />
