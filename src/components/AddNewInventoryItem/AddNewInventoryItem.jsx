330511a7ec5dc6fd20d2f5cfacd4b6671e0a265c
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-03-23 16:40:19 -0700

    update AddNewInventoryItem.jsx to make axios call and return warehouses array on load

--- src/components/AddNewInventoryItem/AddNewInventoryItem.jsx
+++ src/components/AddNewInventoryItem/AddNewInventoryItem.jsx
@@ -1,9 +1,37 @@
 import "./AddNewInventoryItem.scss";
+import axios from "axios";
+import { useState, useEffect } from 'react';
 import ArrowBack from "../../assets/icons/arrow_back-24px.svg";
 import ItemDetailsForm from "../ItemDetailsForm/ItemDetailsForm";
 import ItemAvailabilityForm from "../ItemAvailabilityForm/ItemAvailabilityForm";
 
 function AddNewInventoryItem() {
+
+//USEEFFECT TO GET WAREHOUSE INFORMATION  -  LATER GET PASSED DOWN
+
+const api=process.env.REACT_APP_BASEURL;
+//on load get warehouses
+useEffect(() => {
+  getWarehouses()
+}, []);
+
+//api get call function to retrieve single video details and setMainVideo
+function getWarehouses() {
+  axios
+      .get(`${api}/warehouses`)
+      .then((data) => {
+          console.log(data.data)
+      })
+      .catch((err) => {
+          console.log("err: ", err);
+          // navigate("/404");
+      });
+}
+
+
+
+
+
     return (
       <section className="container">
         <div className="heading">
