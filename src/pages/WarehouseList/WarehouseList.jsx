f1a13c983f0545574b2f513ffa38ae3628c4818e
Tatiana Privezentseva <severny.island@gmail.com>
2023-03-24 10:53:54 -0700

    continue working on tablet styling for the component

--- src/pages/WarehouseList/WarehouseList.jsx
+++ src/pages/WarehouseList/WarehouseList.jsx
@@ -3,6 +3,7 @@ import "./WarehouseList.scss";
 import deleteicon from "../../assets/icons/delete_outline-24px.svg";
 import editicon from "../../assets/icons/edit-24px.svg";
 import rightArrow from "../../assets/icons/chevron_right-24px.svg";
+import sorticon from "../../assets/icons/sort-24px.svg";
 export default function WarehouseList() {
   return (
     <section className="warehouseList">
@@ -27,49 +28,69 @@ export default function WarehouseList() {
 
       <ul className="warehouseList__list">
         <li className="warehouseList__items">
-          <div className="warehouseList__item">
-            <div className="warehouseList__left">
-              <div className="warehouseList__warehouse">
-                <div className="warehouseList__subtitle">warehouse</div>
-                <div className="warehouseList__info">
-                  <a
-                    className="warehouseList__link"
-                    href="warehouses/:warehouseId"
-                  >
-                    Manhattan
-                  </a>
-                 
-                    <img className="warehouseList__icon" src={rightArrow} alt="arrow pointing right" />
-             
-                </div>
-              </div>
-              <div className="warehouseList__address">
-                <div className="warehouseList__subtitle">address</div>
-                <div className="warehouseList__info">
-                  503 Broadway, New york, USA
-                </div>
-              </div>
-            </div>
-            <div className="warehouseList__right">
-              <div className="warehouseList__name">
-                <div className="warehouseList__subtitle">contact name</div>
-                <div className="warehouseList__info">Parmin Aujla</div>
-              </div>
-              <div className="warehouseList__contact">
-                <div className="warehouseList__subtitle">
-                  contact information
-                </div>
-                <div className="warehouseList__info">+1 (629) 555-0129</div>
-                <div className="warehouseList__info">paujla@instock.com</div>
-              </div>
-            </div>
+          <div className="warehouseList__list--tablet">
+            <p className="warehouseList__tablet">warehouse</p>
+            <p className="warehouseList__tablet">address</p>
+            <p className="warehouseList__tablet">contact name</p>
+            <p className="warehouseList__tablet">
+              contact information
+              <img
+                className="warehouseList__sort"
+                src={sorticon}
+                alt="sort icon"
+              />
+            </p>
+
+            <p className="warehouseList__tablet">actions</p>
           </div>
-          <div className="warehouseList__icons">
-            <div className="warehouseList__delete">
-              <img src={deleteicon} alt="delete icon" />
+
+          <div className="warehouseList__container">
+            {/* <div className="warehouseList__item">
+              <div className="warehouseList__left"> */}
+            <div className="warehouseList__item">
+              <div className="warehouseList__subtitle">warehouse</div>
+              <div className="warehouseList__info">
+                <a
+                  className="warehouseList__link"
+                  href="warehouses/:warehouseId"
+                >
+                  Manhattan
+                </a>
+
+                <img
+                  className="warehouseList__icon"
+                  src={rightArrow}
+                  alt="arrow pointing right"
+                />
+              </div>
             </div>
-            <div className="warehouseList__edit">
-              <img src={editicon} alt="edit icon" />
+            <div className="warehouseList__item">
+              <div className="warehouseList__subtitle">address</div>
+              <div className="warehouseList__info">
+                <p>503 Broadway,</p>
+                <p> New york, USA</p>
+              </div>
+            </div>
+            {/* </div> */}
+            {/* <div className="warehouseList__right"> */}
+            <div className="warehouseList__item">
+              <div className="warehouseList__subtitle">contact name</div>
+              <div className="warehouseList__info">Parmin Aujla</div>
+            </div>
+            <div className="warehouseList__item">
+              <div className="warehouseList__subtitle">contact information</div>
+              <div className="warehouseList__info">+1 (629) 555-0129</div>
+              <div className="warehouseList__info">paujla@instock.com</div>
+            </div>
+            {/* </div> */}
+            {/* </div> */}
+            <div className="warehouseList__item warehouseList__item--last">
+              <div className="warehouseList__delete">
+                <img src={deleteicon} alt="delete icon" />
+              </div>
+              <div className="warehouseList__edit">
+                <img src={editicon} alt="edit icon" />
+              </div>
             </div>
           </div>
         </li>
