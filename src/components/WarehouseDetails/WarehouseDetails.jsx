ab94405410ff39de2e88272228cf2b71c8464721
Genesis Vivas <genesis.vivasb@gmail.com>
2023-03-25 23:58:58 -0700

    Fix styles

--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -32,54 +32,52 @@ function WarehouseDetails() {
   if (!Object.keys(warehouse).length) return;
 
   return (
-    <>
-      <section className="warehouse">
-        <div className="warehouse__header">
-          <div className="warehouse__header-wrapper">
-            <Link to="/warehouses">
-              <img src={arrowBackIcon} alt="arrow back" />
-            </Link>
-            <h1>{warehouse.warehouse_name}</h1>
-          </div>
-
-          <Link
-            className="warehouse__edit-button"
-            to={`/warehouses/edit/${warehouseId}`}
-          >
-            <img src={editIconWhite} alt="edit icon" />
-            <h3 className="warehouse__edit-button-caption">Edit</h3>
+    <section className="warehouse">
+      <div className="warehouse__header">
+        <div className="warehouse__header-wrapper">
+          <Link to="/warehouses">
+            <img src={arrowBackIcon} alt="arrow back" />
           </Link>
+          <h1>{warehouse.warehouse_name}</h1>
         </div>
 
-        <div className="warehouse-info">
-          <div className="warehouse-info__address-wrapper">
-            <h4 className="warehouse__sub-heading">Warehouse Address:</h4>
-            <div>
-              <p className="warehouse-info__address">
-                {warehouse.address}
-                {", "}
-              </p>
-              <p className="warehouse-info__address">{warehouse.city}</p>
-            </div>
+        <Link
+          className="warehouse__edit-button"
+          to={`/warehouses/edit/${warehouseId}`}
+        >
+          <img src={editIconWhite} alt="edit icon" />
+          <h3 className="warehouse__edit-button-caption">Edit</h3>
+        </Link>
+      </div>
+
+      <div className="warehouse-info">
+        <div className="warehouse-info__address-wrapper">
+          <h4 className="warehouse__sub-heading">Warehouse Address:</h4>
+          <div>
+            <p className="warehouse-info__address">
+              {warehouse.address}
+              {", "}
+            </p>
+            <p className="warehouse-info__address">{warehouse.city}</p>
           </div>
-          <div className="warehouse-info__contact">
-            <div className="warehouse-info__contact-wrapper">
-              <h4 className="warehouse__sub-heading">Contact Name:</h4>
-              <p>{warehouse.contact_name}</p>
-              <p>{warehouse.contact_position}</p>
-            </div>
-            <div className="warehouse-info__contact warehouse-info__contact--right">
-              <div>
-                <h4 className="warehouse__sub-heading">Contact Information:</h4>
-                <p>{warehouse.contact_phone}</p>
-                <p>{warehouse.contact_email}</p>
-              </div>
+        </div>
+        <div className="warehouse-info__contact">
+          <div className="warehouse-info__contact-wrapper">
+            <h4 className="warehouse__sub-heading">Contact Name:</h4>
+            <p>{warehouse.contact_name}</p>
+            <p>{warehouse.contact_position}</p>
+          </div>
+          <div className="warehouse-info__contact warehouse-info__contact--right">
+            <div>
+              <h4 className="warehouse__sub-heading">Contact Information:</h4>
+              <p>{warehouse.contact_phone}</p>
+              <p>{warehouse.contact_email}</p>
             </div>
           </div>
         </div>
-      </section>
+      </div>
       <WarehouseInventoryList />
-    </>
+    </section>
   );
 }
 
