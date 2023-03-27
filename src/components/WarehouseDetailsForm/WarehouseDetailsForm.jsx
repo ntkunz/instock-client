fa221a7b550091b5aa6f356cfe29250751eec525
TommyNguyen <timsnguyen@hotmail.com>
2023-03-26 20:55:38 -0700

    finish error states

--- src/components/WarehouseDetailsForm/WarehouseDetailsForm.jsx
+++ src/components/WarehouseDetailsForm/WarehouseDetailsForm.jsx
@@ -1,88 +1,101 @@
 import "./WarehouseDetailsForm.scss";
-import ErrorLogo from "../../assets/icons/error-24px.svg";
+import ErrorStateForm from "../ErrorStateForm/ErrorStateForm";
 
-function WarehouseDetailsForm({ formData, handleChange, errorValues }) {
+function WarehouseDetailsForm({
+  handleChangeWarehouse,
+  handleChangeStreetAddress,
+  handleChangeCity,
+  handleChangeCountry,
+  submit,
+  warehouse,
+  streetAddress,
+  city,
+  country,
+}) {
   return (
     <div className="warehouse-details">
       <div className="warehouse-details__container">
         <div className="warehouse-details__warehouse">
           <h2 className="warehouse-details__heading">Warehouse Details</h2>
-    
-          <label htmlFor="warehouseName" className="warehouse-details__label">Warehouse Name</label>
+
+          <label htmlFor="warehouseName" className="warehouse-details__label">
+            Warehouse Name
+          </label>
           <input
-            value={formData.warehouse}
-            className="warehouse-details__input"
+            value={warehouse}
+            className={
+              submit === true && !warehouse
+                ? "warehouse-details__input--error"
+                : "warehouse-details__input"
+            }
             placeholder="Warehouse Name"
             type="text"
             name="warehouse"
             id="warehouse"
-            onChange={handleChange}
+            onChange={handleChangeWarehouse}
           />
-          {errorValues.empty && (
-            <div className="error-container">
-            <img src={ErrorLogo} alt="Error enter valid email" />
-          <p className="error-container__message">Required field.</p>
-          </div>
-          )}
+          {submit === true && !warehouse === true && <ErrorStateForm />}
         </div>
 
         <div className="warehouse-details__warehouse">
-          <label htmlFor="streetAddress" className="warehouse-details__label">Street Address</label>
+          <label htmlFor="streetAddress" className="warehouse-details__label">
+            Street Address
+          </label>
           <input
-            value={formData.streetAddress}
-            className="warehouse-details__input"
-            placeholder="Street Address"
+            value={streetAddress}
+            className={
+              submit === true && !warehouse
+                ? "warehouse-details__input--error"
+                : "warehouse-details__input"
+            }
+            placeholder="Street Adress"
             type="text"
             name="streetAddress"
             id="streetAddress"
-            onChange={handleChange}
+            onChange={handleChangeStreetAddress}
           />
-          {errorValues.empty && (
-            <div className="error-container">
-            <img src={ErrorLogo} alt="Error enter valid email" />
-          <p className="error-container__message">Required field.</p>
-          </div>
-          )}
+          {submit === true && !streetAddress === true && <ErrorStateForm />}
         </div>
 
         <div className="warehouse-details__warehouse">
-          <label htmlFor="city" className="warehouse-details__label">City</label>
+          <label htmlFor="city" className="warehouse-details__label">
+            City
+          </label>
           <input
-            value={formData.city}
-            className="warehouse-details__input"
+            value={city}
+            className={
+              submit === true && !warehouse
+                ? "warehouse-details__input--error"
+                : "warehouse-details__input"
+            }
             placeholder="City"
             type="text"
             id="city"
             name="city"
-            onChange={handleChange}
+            onChange={handleChangeCity}
           />
-          {errorValues.empty && (
-            <div className="error-container">
-            <img src={ErrorLogo} alt="Error enter valid email" />
-          <p className="error-container__message">Required field.</p>
-          </div>
-          )}
+          {submit === true && !city === true && <ErrorStateForm />}
         </div>
 
         <div className="warehouse-details__warehouse">
-          <label htmlFor="country" className="warehouse-details__label">Country</label>
+          <label htmlFor="country" className="warehouse-details__label">
+            Country
+          </label>
           <input
-            value={formData.country}
-            className="warehouse-details__input"
+            value={country}
+            className={
+              submit === true && !warehouse
+                ? "warehouse-details__input--error"
+                : "warehouse-details__input"
+            }
             placeholder="Country"
             type="text"
             id="country"
             name="country"
-            onChange={handleChange}
+            onChange={handleChangeCountry}
           />
-          {errorValues.empty && (
-            <div className="error-container">
-            <img src={ErrorLogo} alt="Error enter valid email" />
-          <p className="error-container__message">Required field.</p>
-          </div>
-          )}
+          {submit === true && !country === true && <ErrorStateForm />}
         </div>
-
       </div>
     </div>
   );
