e395fd7f5500d28ef2dff9c4bb538132397ec7ed
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-03-27 11:46:01 -0700

    refactor EditInventory.jsx

--- src/components/EditItemAvailabilityForm/EditItemAvailabilityForm.jsx
+++ src/components/EditItemAvailabilityForm/EditItemAvailabilityForm.jsx
@@ -1,9 +1,25 @@
 import "./EditItemAvailabilityForm.scss";
+import InventoryItemErrorMessage from "../InventoryItemErrorMessage/InventoryItemErrorMessage";
 
-function EditItemAvailabilityForm({ warehouses, handleOnChange, inputValues }) {
+
+function EditItemAvailabilityForm({ 
+	
+	warehouses, 
+	status, 
+	quantity, 
+	selectWarehouse, 
+	handleChangeStatus, 
+	handleChangeQuantity, 
+	handleChangeSelectWarehouse,
+	statusError,
+	quantityError,
+	selectWarehouseError,
+	submit }) {
+
 	return (
 		<div className="avail">
-		<h2 className="avail__title">Item Availability</h2>
+			<h2 className="avail__title">Item Availability</h2>
+
 			<label htmlFor="name" className="avail__label">
 				Status
 			</label>
@@ -13,10 +29,11 @@ function EditItemAvailabilityForm({ warehouses, handleOnChange, inputValues }) {
 						className="avail__radio"
 						type="radio"
 						id="instock"
-						name="instock"
+						name="status"
 						value="In Stock"
-						onChange={handleOnChange}
+						onChange={handleChangeStatus}
 					/>
+
 					<label className="avail__radio-label" htmlFor="instock">
 						In stock
 					</label>
@@ -26,28 +43,34 @@ function EditItemAvailabilityForm({ warehouses, handleOnChange, inputValues }) {
 						className="avail__radio"
 						type="radio"
 						id="outofstock"
-						name="instock"
+						name="status"
 						value="Out of Stock"
-						onChange={handleOnChange}
+						onChange={handleChangeStatus}
 					/>
-					<label className="avail__radio-label" htmlFor="outofstock">
+					<label
+						className="avail__radio-label out-of-stock"
+						htmlFor="outofstock"
+					>
 						Out of stock
 					</label>
+					{submit === true && statusError === true && <InventoryItemErrorMessage />}
 				</div>
 			</div>
-			<div className="avail__quantity-wrap avail__out-of-stock">
-				<label htmlFor="quantity" className="avail__label">
-					Quantity
-				</label>
-				<input
-					type="text"
-					className="avail__input"
-					value={inputValues.quantity}
-					onChange={handleOnChange}
-					placeholder=""
-					name="quantity"
-				/>
-			</div>
+		<div className="avail__quantity-wrap avail__out-of-stock">
+			<label htmlFor="quantity" className="avail__label">
+				Quantity
+			</label>
+			<input
+				type="text"
+				className="avail__input"
+				value={quantity}
+				onChange={handleChangeQuantity}
+				placeholder=""
+				name="quantity"
+			/>
+		</div>
+		{submit === true && quantityError === true && <InventoryItemErrorMessage />}
+
 			<label htmlFor="" className="avail__label">
 				Warehouse
 			</label>
@@ -56,21 +79,112 @@ function EditItemAvailabilityForm({ warehouses, handleOnChange, inputValues }) {
 					className="avail__warehouse"
 					name="selectWarehouse"
 					id="avail_warehouse"
-					onChange={handleOnChange}
-					value={inputValues.selectWarehouse}
+					onChange={handleChangeSelectWarehouse}
+					value={selectWarehouse}
 				>
-					<option value={inputValues.selectWarehouse}>
-						{inputValues.selectWarehouse}
+					<option value="" readOnly>
+						Please select
 					</option>
 					{warehouses.map((warehouse) => (
-						<option key={warehouse.id} value={warehouse.warehouse_name}>
+						<option 
+							key={warehouse.id} 
+							value={warehouse.id}>
 							{warehouse.warehouse_name}
 						</option>
 					))}
 				</select>
 			</div>
+			{submit === true && selectWarehouseError === true && <InventoryItemErrorMessage />}
 		</div>
 	);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	// warehouses, handleOnChange, inputValues }) {
+	// return (
+	// 	<div className="avail">
+	// 	<h2 className="avail__title">Item Availability</h2>
+	// 		<label htmlFor="name" className="avail__label">
+	// 			Status
+	// 		</label>
+	// 		<div className="avail__radio-container">
+	// 			<div className="avail__radio-set">
+	// 				<input
+	// 					className="avail__radio"
+	// 					type="radio"
+	// 					id="instock"
+	// 					name="instock"
+	// 					value="In Stock"
+	// 					onChange={handleOnChange}
+	// 				/>
+	// 				<label className="avail__radio-label" htmlFor="instock">
+	// 					In stock
+	// 				</label>
+	// 			</div>
+	// 			<div className="avail__radio-set">
+	// 				<input
+	// 					className="avail__radio"
+	// 					type="radio"
+	// 					id="outofstock"
+	// 					name="instock"
+	// 					value="Out of Stock"
+	// 					onChange={handleOnChange}
+	// 				/>
+	// 				<label className="avail__radio-label" htmlFor="outofstock">
+	// 					Out of stock
+	// 				</label>
+	// 			</div>
+	// 		</div>
+	// 		<div className="avail__quantity-wrap avail__out-of-stock">
+	// 			<label htmlFor="quantity" className="avail__label">
+	// 				Quantity
+	// 			</label>
+	// 			<input
+	// 				type="text"
+	// 				className="avail__input"
+	// 				value={inputValues.quantity}
+	// 				onChange={handleOnChange}
+	// 				placeholder=""
+	// 				name="quantity"
+	// 			/>
+	// 		</div>
+	// 		<label htmlFor="" className="avail__label">
+	// 			Warehouse
+	// 		</label>
+	// 		<div className="avail__select-wrap">
+	// 			<select
+	// 				className="avail__warehouse"
+	// 				name="selectWarehouse"
+	// 				id="avail_warehouse"
+	// 				onChange={handleOnChange}
+	// 				value={inputValues.selectWarehouse}
+	// 			>
+	// 				<option value={inputValues.selectWarehouse}>
+	// 					{inputValues.selectWarehouse}
+	// 				</option>
+	// 				{warehouses.map((warehouse) => (
+	// 					<option key={warehouse.id} value={warehouse.warehouse_name}>
+	// 						{warehouse.warehouse_name}
+	// 					</option>
+	// 				))}
+	// 			</select>
+	// 		</div>
+	// 	</div>
+	// );
+
+
+
+
+
+
 }
 
 export default EditItemAvailabilityForm;
--- src/pages/EditInventory/EditInventory.jsx
+++ src/pages/EditInventory/EditInventory.jsx
@@ -7,93 +7,91 @@ import EditItemDetailsForm from "../../components/EditItemDetailsForm/EditItemDe
 import EditItemAvailabilityForm from "../../components/EditItemAvailabilityForm/EditItemAvailabilityForm";
 
 function EditInventory() {
-//bring in Api address for axios calls
-const api = process.env.REACT_APP_BASEURL;
-const { v4 } = require("uuid");
-const navigate = useNavigate();
+	//bring in Api address for axios calls
+	const api = process.env.REACT_APP_BASEURL;
+	const { v4 } = require("uuid");
+	const navigate = useNavigate();
+	const id = useParams();
 
-const [warehouses, setWarehouses] = useState([]);
-const [inventories, setInventories] = useState([]);
-const [itemName, setItemName] = useState("");
-const [desc, setDesc] = useState("");
-const [category, setCategory] = useState("");
-const [status, setStatus] = useState("");
-const [quantity, setQuantity] = useState("");
-const [selectWarehouse, setSelectWarehouse] = useState("");
-const [itemNameError, setItemNameError] = useState(false);
-const [descError, setDescError] = useState(false);
-const [selectWarehouseError, setSelectWarehouseError] = useState(false);
-const [categoryError, setCategoryError] = useState(false);
-const [statusError, setStatusError] = useState(false);
-const [quantityError, setQuantityError] = useState(false);
-const [submit, setSubmit] = useState(false);
-const [empty, setEmpty] = useState(false);
-const [add, setAdd] = useState(false);
+	const [warehouses, setWarehouses] = useState([]);
+	const [inventories, setInventories] = useState([]);
+	const [itemName, setItemName] = useState("");
+	const [desc, setDesc] = useState("");
+	const [category, setCategory] = useState("");
+	const [status, setStatus] = useState("");
+	const [quantity, setQuantity] = useState("");
+	const [selectWarehouse, setSelectWarehouse] = useState("");
+	const [itemNameError, setItemNameError] = useState(false);
+	const [descError, setDescError] = useState(false);
+	const [selectWarehouseError, setSelectWarehouseError] = useState(false);
+	const [categoryError, setCategoryError] = useState(false);
+	const [statusError, setStatusError] = useState(false);
+	const [quantityError, setQuantityError] = useState(false);
+	const [submit, setSubmit] = useState(false);
+	const [empty, setEmpty] = useState(false);
+	const [add, setAdd] = useState(false);
 
+	const handleChangeSelectWarehouse = (event) => {
+		if (selectWarehouse !== "") setSelectWarehouseError(false);
+		document.querySelector(".avail__warehouse").classList.remove("error");
+		setSelectWarehouse(event.target.value);
+	};
 
-const handleChangeSelectWarehouse = (event) => {
-    if (selectWarehouse !== '') setSelectWarehouseError(false);
-	document.querySelector('.avail__warehouse').classList.remove('error')
-	setSelectWarehouse(event.target.value);
-  };
+	const handleChangeItemName = (event) => {
+		if (itemName !== "") setItemNameError(false);
+		document.querySelector(".details__input").classList.remove("error");
+		setItemName(event.target.value);
+	};
 
-  const handleChangeItemName = (event) => {
-	if (itemName !== '') setItemNameError(false)
-	document.querySelector('.details__input').classList.remove('error')
-    setItemName(event.target.value);
-  };
+	const handleChangeDesc = (event) => {
+		if (desc !== "") setDescError(false);
+		document.querySelector(".details__desc-input").classList.remove("error");
+		setDesc(event.target.value);
+	};
 
-  const handleChangeDesc = (event) => {
-	if (desc !== '') setDescError(false)
-	document.querySelector('.details__desc-input').classList.remove('error')
-    setDesc(event.target.value);
-  };
+	const handleChangeCategory = (event) => {
+		if (category !== "") setCategoryError(false);
+		document.querySelector(".details__select").classList.remove("error");
+		setCategory(event.target.value);
+	};
 
-  const handleChangeCategory = (event) => {
-    if (category !== '') setCategoryError(false)
-	document.querySelector('.details__select').classList.remove('error')
-	setCategory(event.target.value);
-  };
+	const handleChangeQuantity = (event) => {
+		if (quantity !== "") setQuantityError(false);
+		document.querySelector(".avail__input").classList.remove("error");
+		setQuantity(event.target.value);
+	};
 
-  const handleChangeQuantity = (event) => {
-	if (quantity !== '') setQuantityError(false)
-	document.querySelector('.avail__input').classList.remove('error')
-    setQuantity(event.target.value);
-  };
+	const handleChangeStatus = (event) => {
+		//create variables for checkbox radios
+		const quantityWrapper = document.querySelector(".avail__quantity-wrap");
+		const stockRadio = document.querySelectorAll(".avail__radio");
+		//create true value if item in stock, false if out of stock
+		let stockCheck = stockRadio[0].checked;
+		//if item in stock then set instock inputValue and show quantity field
+		if (stockCheck) {
+			quantityWrapper.classList.remove("avail__out-of-stock");
+		}
+		//if item out of stock then set instock inputVluae and hide quantity field
+		if (!stockCheck) {
+			// setStatusError(true)
+			quantityWrapper.classList.add("avail__out-of-stock");
+		}
+		setStatus(event.target.value);
+	};
 
-  const handleChangeStatus = (event) => {
-	//create variables for checkbox radios
-	const quantityWrapper = document.querySelector(".avail__quantity-wrap");
-	const stockRadio = document.querySelectorAll(".avail__radio");
-	//create true value if item in stock, false if out of stock
-	let stockCheck = stockRadio[0].checked;
-	//if item in stock then set instock inputValue and show quantity field
-	if (stockCheck) {
-		quantityWrapper.classList.remove("avail__out-of-stock");
-	}
-	//if item out of stock then set instock inputVluae and hide quantity field
-	if (!stockCheck) {
-		// setStatusError(true)
-		quantityWrapper.classList.add("avail__out-of-stock");
-	}
-    setStatus(event.target.value);
-  };
-
-
-  const isFormValid = () => {
-    if (
-      selectWarehouse === "" ||
-      itemName === "" ||
-      desc === "" ||
-      category === "" ||
-      status === "" ||
-      quantity === "" 
-    ) {
-      return setEmpty(true);
-    }
-    setEmpty(false);
-  }
-
+	const isFormValid = () => {
+		if (
+			selectWarehouse === "" ||
+			itemName === "" ||
+			desc === "" ||
+			category === "" ||
+			status === "" ||
+			quantity === ""
+		) {
+			return setEmpty(true);
+		}
+		setEmpty(false);
+	};
 
 	//on load get warehouses and inventories
 	useEffect(() => {
@@ -152,12 +150,12 @@ const handleChangeSelectWarehouse = (event) => {
 				if (data) {
 					const inventoryItem = data.data;
 
-					setSelectWarehouse(inventoryItem.warehouse_name)
-					setItemName(inventoryItem.item_name)
-					setDesc(inventoryItem.description)
-					setCategory(inventoryItem.category)
-					setStatus(inventoryItem.status)
-					setQuantity(inventoryItem.quantity)
+					setSelectWarehouse(inventoryItem.warehouse_name);
+					setItemName(inventoryItem.item_name);
+					setDesc(inventoryItem.description);
+					setCategory(inventoryItem.category);
+					setStatus(inventoryItem.status);
+					setQuantity(inventoryItem.quantity);
 				}
 			})
 			.catch((err) => {
@@ -165,30 +163,104 @@ const handleChangeSelectWarehouse = (event) => {
 			});
 	}
 
+	//function to handle form submit
+	function handleFormSubmit(e) {
+		e.preventDefault();
+		setSubmit(true);
+
+		if (itemName === "") {
+			setItemNameError(true);
+			document.querySelector(".details__input").classList.add("error");
+			return alert("Please enter an item name");
+		}
+
+		// if (inputValues.desc === '') {
+		if (desc === "") {
+			setDescError(true);
+			document.querySelector(".details__desc-input").classList.add("error");
+			return alert("Please enter an item description");
+		}
+		// if (inputValues.category === '') {
+		if (category === "") {
+			setCategoryError(true);
+			document.querySelector(".details__select").classList.add("error");
+			return alert("Please select a category");
+		}
+
+		// if (isNaN(inputValues.quantity) || inputValues.quantity === '') {
+		if (isNaN(quantity) || quantity === "") {
+			setQuantityError(true);
+			document.querySelector(".avail__input").classList.add("error");
+			return alert("Please enter a number for quantity");
+		}
+
+		// if (inputValues.selectWarehouse === '') {
+		if (selectWarehouse === "") {
+			setSelectWarehouseError(true);
+			document.querySelector(".avail__warehouse").classList.add("error");
+			return alert("Please select a warehouse");
+		}
+
+		//set new id variable to be able to navigate to page after
+		let newId = v4();
+
+		if (isFormValid()) {
+			axios
+				.post(`${api}/inventories`, {
+					id: newId,
+					warehouse_id: selectWarehouse,
+					item_name: itemName,
+					description: desc,
+					category: category,
+					status: status,
+					quantity: quantity,
+				})
+				.then((response) => {
+					alert(`Your new inventory item ${itemName} has been added`);
+
+					//reset form on successful submit
+					e.target.reset();
+					setSelectWarehouse("");
+					setItemName("");
+					setDesc("");
+					setCategory("");
+					setStatus("");
+					setQuantity(0);
+					navigate(`/inventory/${newId}`);
+				})
+				.catch((err) => {
+					console.error(err);
+				});
+		}
+	}
+
 	//function to handle form cancel
 	function handleFormCancel(e) {
 		e.preventDefault();
 		document.querySelector("form").reset();
-		setSelectWarehouse("")
-		setItemName("")
-		setDesc("")
-		setCategory("")
-		setStatus("")
-		setQuantity(0)
-		navigate(-1)
+		setSelectWarehouse("");
+		setItemName("");
+		setDesc("");
+		setCategory("");
+		setStatus("");
+		setQuantity(0);
+		navigate(-1);
 	}
 
-		return (
+	return (
 		<section className="container">
 			<div className="heading">
-				<Link className="heading__link" to={".."} onClick={(e) => {
+				<Link
+					className="heading__link"
+					to={".."}
+					onClick={(e) => {
 						e.preventDefault();
 						navigate(-1);
 					}}
 				>
 					<img src={ArrowBack} alt="ArrowBackButton" />
 				</Link>
-				<h1 className="heading__title">Add New Inventory Item</h1>
+				<h1 className="heading__title">Edit Inventory Item</h1>
 			</div>
 			<form className="form" onSubmit={handleFormSubmit}>
 				<div className="form__component-container">
@@ -230,34 +302,12 @@ const handleChangeSelectWarehouse = (event) => {
 						>
 							Cancel
 						</button>
-						<button className="form__button form__button--2">+ Add Item</button>
+						<button className="form__button form__button--2">+ Edit Item</button>
 					</div>
 				</div>
 			</form>
 		</section>
 	);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 	// //bring in Api address for axios calls
 	// const api = process.env.REACT_APP_BASEURL;
